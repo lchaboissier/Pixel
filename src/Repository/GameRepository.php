b5ec61e6154aaa38c95eb9596b3ea57c022b82a4
Luca Chaboissier <chaboiss6u@etu.univ-lorraine.fr>
2022-09-14 17:46:25 +0200

    GameRepository added

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -3,8 +3,12 @@
 namespace App\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 class GameRepository extends ServiceEntityRepository
 {
-    
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Game::class);
+    }
 }
